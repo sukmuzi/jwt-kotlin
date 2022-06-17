6f32a0f13a6891ed99b4ad0a9806dfdc2b4cfe2c
sukmuzi <sukmuzi@naver.com>
2022-06-17 17:53:19 +0900

    (java to kotlin) lombok 삭제

--- src/main/kotlin/com/example/jwtkotlin/entity/Authority.kt
+++ src/main/kotlin/com/example/jwtkotlin/entity/Authority.kt
@@ -1,6 +1,5 @@
 package com.example.jwtkotlin.entity
 
-import lombok.Builder
 import javax.persistence.Column
 import javax.persistence.Entity
 import javax.persistence.Id
@@ -8,7 +7,6 @@ import javax.persistence.Table
 
 @Entity
 @Table(name = "tb_authority")
-@Builder
 data class Authority(
 
     @Id
